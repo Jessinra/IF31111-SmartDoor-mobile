ccc4b400d59e383f79916554b3ecbbe9f2ae62b3
JessinRa <jessin.bdg@gmail.com>
2019-02-23 22:27:02 +0700

    Final card UI for dashboard
    - create 10 template, added example how to change text, lock state, and visibility
    - cardviewAdaptor -> adaptor class to change view text / image state
    - dummybuilding to create dummy payload

--- app/src/main/java/com/knockknock/dragonra/smartdoor/view/DashboardFragment/DashboardFragment.java
+++ app/src/main/java/com/knockknock/dragonra/smartdoor/view/DashboardFragment/DashboardFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,10 @@ import android.view.ViewGroup;
 
 import com.knockknock.dragonra.smartdoor.LogoutActivity;
 import com.knockknock.dragonra.smartdoor.R;
+import com.knockknock.dragonra.smartdoor.model.dummy.DummyBuilding;
+import com.knockknock.dragonra.smartdoor.view.Component.CardViewAdaptor;
+
+import java.util.ArrayList;
 
 public class DashboardFragment extends Fragment implements View.OnClickListener {
 
@@ -41,14 +46,46 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         view.findViewById(R.id.button_sign_out).setOnClickListener(this);
+
+        modifyCardView(view);
+    }
+
+    private void modifyCardView(@NonNull View view) {
+
+        ViewGroup dashboardCard = view.findViewById(R.id.dashboard);
+        View dashboardChildView;
+
+        // TODO: example of handling data payload
+        ArrayList<DummyBuilding> buildings = DummyBuilding.createMockBuildings(10);
+        int j = 0;
+        DummyBuilding building;
+
+        // Iterate through all cardView
+        for (int i = 0; i < dashboardCard.getChildCount(); i++) {
+            dashboardChildView = dashboardCard.getChildAt(i);
+            if (dashboardChildView instanceof CardView) {
+
+                // Pick the dummy data
+                building = buildings.get(j);
+                j++;
+
+                // TODO: do something with the card view !
+                CardViewAdaptor.setText(dashboardChildView, building.getName());
+                CardViewAdaptor.setLockState(dashboardChildView, building.isLocked());
+//                CardViewAdaptor.setInvisible(dashboardChildView);
+
+            }
+        }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+
             case R.id.button_sign_out:
                 redirectGoogleSignOut();
                 break;
+
             default:
                 break;
         }
@@ -60,5 +97,6 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
         Intent intent = new Intent(getActivity(), LogoutActivity.class);
         startActivity(intent);
     }
+
 
 }
