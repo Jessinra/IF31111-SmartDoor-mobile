c1b026b8103bb06f71881f889e42878242606bb6
cks <ogatatsumi97@yahoo.com>
2019-02-25 20:26:27 +0700

    add delete user function

--- app/src/main/java/com/knockknock/dragonra/smartdoor/view/Adapter/RegisterUserPageAdapter.java
+++ app/src/main/java/com/knockknock/dragonra/smartdoor/view/Adapter/RegisterUserPageAdapter.java
@@ -3,11 +3,18 @@ package com.knockknock.dragonra.smartdoor.view.Adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.knockknock.dragonra.smartdoor.R;
 
 import java.util.LinkedList;
@@ -16,7 +23,7 @@ public class RegisterUserPageAdapter extends RecyclerView.Adapter<RegisterUserPa
 
     private final LinkedList<String> mMemList;
     private LayoutInflater mInflater;
-
+    private static final String TAG = "RegisterUserPageAdapter";
     public RegisterUserPageAdapter(Context context,
                                    LinkedList<String> memList) {
         mInflater = LayoutInflater.from(context);
@@ -40,7 +47,7 @@ public class RegisterUserPageAdapter extends RecyclerView.Adapter<RegisterUserPa
         return mMemList.size();
     }
 
-    class RegViewHolder extends RecyclerView.ViewHolder {
+    class RegViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         final TextView wordItemView;
         final RegisterUserPageAdapter mAdapter;
 
@@ -48,6 +55,29 @@ public class RegisterUserPageAdapter extends RecyclerView.Adapter<RegisterUserPa
             super(itemView);
             wordItemView = itemView.findViewById(R.id.word);
             this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Log.wtf(TAG, "onClick: testing");
+            int mPosition = getLayoutPosition();
+            String element = mMemList.get(mPosition);
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+            Query removeQuery = ref.child("member").orderByChild("name").equalTo(element);
+            removeQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot ds: dataSnapshot.getChildren()) {
+                        ds.getRef().removeValue();
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.e(TAG, "onCancelled", databaseError.toException());
+                }
+            });
         }
     }
 }
